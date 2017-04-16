9e6d628ab62d06e8459bfdd041a01385d12588d0
Kevin Liu <kevin_liu@consultant.com>
2017-04-15 23:23:51 -0400

    play_song modified

--- MP3/play_song.py
+++ MP3/play_song.py
@@ -8,7 +8,9 @@ pygame.mixer.music.load(sys.argv[1])
 try:
     while True:
         pygame.mixer.music.play()
+        #pygame.event.wait()  # CTRL-C won't work
         while pygame.mixer.music.get_busy(): 
-            pygame.time.Clock().tick(1)
+            #pygame.time.Clock().tick(1)
+            pass
 except KeyboardInterrupt:
     sys.exit("\nBye!")
